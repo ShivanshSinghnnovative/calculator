92298f2193286e897933f240a2e90892fd3c6502
ShivanshSinghnnovative <shivansh.singh@innovative.in>
2023-09-04 16:12:07 +0530

    update changes

--- script.js
+++ script.js
@@ -1,129 +1,117 @@
-
-var currentValue='';
-let signPresent=0;
+let currentValue = "";
+let signPresent = 0;
+let display, value, operators, operands, result;
 function appendToDisplay(value) {
-    
-    if((value==="+" || value==="-" || value==="*" || value==="/") && signPresent===0){
-        var display = document.getElementById('display');
-        // console.log(signPresent + "h");
-        // console.log("kk");
+  if (
+    (value === "+" || value === "-" || value === "*" || value === "/") &&
+    signPresent === 0
+  ) {
+    display = document.getElementById("display");
+    // console.log(signPresent + "h");
+    // console.log("kk");
     display.value += value;
-    // currentValue+=value; 
-       currentValue='';
-       signPresent=1;
-       console.log(signPresent +'2');
+    // currentValue+=value;
+    currentValue = "";
+    signPresent = 1;
+    console.log(signPresent + "2");
+  } else {
+    if (!(value === "+" || value === "-" || value === "*" || value === "/")) {
+      display = document.getElementById("display");
+      display.value += value;
+      currentValue += value;
+      signPresent = 0;
     }
-    else{
-
-        if( !(value==="+" || value==="-" || value==="*" || value==="/") ){
-        var display = document.getElementById('display');
-    display.value += value;
-    currentValue+=value;
-        signPresent=0;
-    }
-}
-    
-
+  }
 }
 
-function appendToDecimal(value){
-    if(currentValue.length==0){
-        var display = document.getElementById('display');
-        display.value += '0';
-        display.value += value;
-        currentValue+=value;
-    }
-    if (!currentValue.includes('.')) {
-        var display = document.getElementById('display');
-        display.value += value;
-        currentValue+=value;
-      }
+function appendToDecimal(value) {
+  if (currentValue.length == 0) {
+    display = document.getElementById("display");
+    display.value += "0";
+    display.value += value;
+    currentValue += value;
+  }
+  if (!currentValue.includes(".")) {
+    display = document.getElementById("display");
+    display.value += value;
+    currentValue += value;
+  }
 }
 
 function clearDisplay() {
-    display.value = "";
-    currentValue='';
-    signPresent=0;
+  display.value = "";
+  currentValue = "";
+  signPresent = 0;
 }
-
 
 function calculateResult() {
-    var value = document.getElementById('display').value;
-    var result = customEval(value);
-    document.getElementById('display').value = result;
+  value = document.getElementById("display").value;
+  result = customEval(value);
+  document.getElementById("display").value = result;
 }
-
 
 function customEval(value) {
+  operators = value.match(/[+\-*/]/g);
+  operands = value.split(/[+\-*/]/).map(function (item) {
+    console.log(item);
+    return parseFloat(item);
+  });
+  let oper = [];
 
-  
-
-  
-    var operators = value.match(/[+\-*/]/g);
-    var operands = value.split(/[+\-*/]/).map(function (item) {
-        console.log(item);
-        return parseFloat(item);
-    });
-    var oper=[];
-    for (var element of operands) {
-        if (typeof element === 'number' && !isNaN(element)) {
-          oper.push(element);
-        }
-      }
-    console.log(oper.length);
-    console.log(operators);
-    console.log(operators.length);
-    console.log(operands);
-    console.log(operands.length);
-
-    if(value[0]=="-" || value[0]=="+"){
-        console.log("jfj");
-        if(value[0]=="+"){
-            operators.splice(0,1);
-        }
-        else{
-            operators.splice(0,1);
-            oper[0]=(-1*oper[0]);
-        }
+  operands.forEach((element) => {
+    if (typeof element === "number" && !isNaN(element)) {
+      oper.push(element);
     }
-    console.log(oper);
-    console.log(operators);
- 
-    for (var i = 0; i < operators.length; i++) {
-        if (operators[i] === "*") {
-            oper[i] = oper[i] * oper[i + 1];
-            oper.splice(i + 1, 1);
-            operators.splice(i, 1);
-            i--;
-        } else if (operators[i] === "/") {
-            oper[i] = oper[i] / oper[i + 1];
-            oper.splice(i + 1, 1);
-            operators.splice(i, 1);
-            i--;
-        }
+  });
+
+  console.log(oper.length);
+  console.log(operators);
+  console.log(operators.length);
+  console.log(operands);
+  console.log(operands.length);
+
+  if (value[0] == "-" || value[0] == "+") {
+    console.log("jfj");
+    if (value[0] == "+") {
+      operators.splice(0, 1);
+    } else {
+      operators.splice(0, 1);
+      oper[0] = -1 * oper[0];
     }
-    var answer = oper[0];
-    for (var i = 0; i < operators.length; i++) {
-        if (operators[i] === "+") {
-            answer += oper[i + 1];
-        } else if (operators[i] === "-") {
-            answer -= oper[i + 1];
-        }
+  }
+  console.log(oper);
+  console.log(operators);
+
+  operators.forEach((operator, i) => {
+    if (operator === "*") {
+      oper[i] = oper[i] * oper[i + 1];
+      oper.splice(i + 1, 1);
+      operators.splice(i, 1);
+    } else if (operator === "/") {
+      oper[i] = oper[i] / oper[i + 1];
+      oper.splice(i + 1, 1);
+      operators.splice(i, 1);
     }
+  });
 
-    
-    if (isNaN(answer) || !isFinite(answer)) {
-      clearAll();
-      document.getElementById("display").innerHTML = "NaN";
-     } 
-  
+  let answer = oper[0];
+  operators.forEach((operator, i) => {
+    if (operator === "+") {
+      answer += oper[i + 1];
+    } else if (operator === "-") {
+      answer -= oper[i + 1];
+    }
+  });
 
+  if (isNaN(answer) || !isFinite(answer)) {
+    clearAll();
+    document.getElementById("display").innerHTML = "NaN";
+  }
 
-    return answer;
+  return answer;
 }
 
-
 function clearAll() {
-    // document.getElementById("display").innerHTML = "";
-    signPresent=0;
+  // document.getElementById("display").innerHTML = "";
+  signPresent = 0;
 }
